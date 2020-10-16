ea4db79d670ecaeddd824e7b94c065c27c5cdbec
maciej790 <maciejmw126@gmail.com>
2020-10-16 14:01:49 +0200

    basic setup project

---
+++ src/components/Clear.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Clear'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <Header />
+        <Input />
+        <TodoItem />
+        <Clear />
+    </div>
+</template>
+
+<script>
+
+import Header from './Header.vue'
+import Input from './Input.vue'
+import TodoItem from './TodoItem.vue'
+import Clear from './Clear.vue'
+
+export default {
+    name: 'Todo',
+    components: {
+        Header,
+        Input,
+        TodoItem,
+        Clear
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
